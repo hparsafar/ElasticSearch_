8fd057e1ee7a085668052b701a54f7560b9d5cf8
hparsafar <parsafar.hoda@gmail.com>
2020-06-27 09:45:26 +0430

    99-04-07 add new project

---
+++ Search/Program.cs
@@ -0,0 +1,33 @@
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Search.Models.Elasticsearch;
+
+namespace Search
+{
+    public class Program
+    {
+        // make sure your program is running C# 7.1 or later
+        public static async Task Main(string[] args)
+        {
+            // setup host
+            var host = CreateWebHostBuilder(args).Build();
+
+            // load records from Csv to Elasticsearch
+            using (var scope = host.Services.CreateScope())
+            {
+                var loader = scope.ServiceProvider.GetRequiredService<CapitalCities>();
+                await loader.RunAsync();
+            }
+
+            // change our run to async
+            await host.RunAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Search/Startup.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nest;
+using Search.Models.Elasticsearch;
+
+namespace Search
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("elasticsearch");
+
+            // 1. Register NEST ElasticClient
+            var settings = new ConnectionSettings(new Uri(connectionString))
+                .DefaultIndex("capitals");
+
+            services.AddSingleton(settings);
+
+            services.AddScoped(s =>
+            {
+                var connectionSettings = s.GetRequiredService<ConnectionSettings>();
+                var client = new ElasticClient(connectionSettings);
+
+                return client;
+            });
+
+
+            services.AddScoped<IFileSystem, FileSystem>();
+            // 2. Register Capital City Loader
+            services.AddScoped<CapitalCities>();
+            
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
